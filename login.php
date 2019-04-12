eb22beb996969be0df6402bcfa5edaa9c480f77e
GeraltMagus <45607751+GeraltMagus@users.noreply.github.com>
2019-04-12 09:59:35 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>MisListas Login</title>
+	<link rel="stylesheet" href="/mislistas/login.css">
+</head>
+<body>
+		<form>
+				<img src="listime.jpeg" class="imgLogin">
+				<br>
+				<label>Sign in</label>
+				<br>
+				<br>
+		<label>Correo:</label>
+		<br>
+		<input type="text" id="txtCorreo" value="correo@dominio.com">
+		<br>
+		<label>Contraseña:</label>
+		<br>
+		<input type="password" id="txtPassword" value="">
+		<br>
+		<br>
+		<input type="button" value="Ingresar">
+		<br>
+		<br>
+		<a href="">¿Olvidaste la contraseña?</a>
+	</form>
+</body>
+</html>
